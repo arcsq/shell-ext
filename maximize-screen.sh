9d8dca031a03cc95e66c341bf9b8010803355aa9
Sunil <sunil@teton.local>
2022-01-20 08:07:51 -0500

    Maximize screen script

---
+++ maximize-screen.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Maximize Current Window
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🤖
+
+yabai -m window --toggle zoom-fullscreen
+
